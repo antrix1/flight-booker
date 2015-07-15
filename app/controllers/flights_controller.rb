0343728c05d3b128d1755ce71dfcb9e3e27605cd
Andrej Dragojevic <andrejdr.93@gmail.com>
2015-07-15 22:18:48 +0200

    Layout and seeded data

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,4 @@
+class FlightsController < ApplicationController
+  def index
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+airports = {:NYC => "New York",
+           :SFO => "San Francisco",
+           :LON => "London",
+           :LPL => "Liverpool",
+           :BEG => "Belgrade",
+           :BER => "Berlin",
+           :ZRH => "Zurich"
+}
+
+airports.each do |k, v|
+    Airport.create(code: k, city: v)
+end
+
+all_airports = Airport.all
+
+all_airports.each_with_index do |airport, index|
+  Flight.create(date: rand(20.days).seconds.from_now,
+                departure_airport_id: airport.id,
+                destination_airport_id: all_airports[index+1].id,
+                price: rand(200..1000)) if index < all_airports.length - 1
+end
+
+
+
+all_airports.to_a.reverse!.each_with_index do |airport, index|
+Flight.create(date: rand(20.days).seconds.from_now,
+              departure_airport_id: airport.id,
+              destination_airport_id: all_airports[index+1].id,
+              price: rand(200..1000)) if index < all_airports.length - 1
+  end
