306a2475017c006f4249caf178845e263731d1ac
Andrej Dragojevic <andrejdr.93@gmail.com>
2015-07-17 02:10:20 +0200

    Added postgres to gemfile and set up production.rb for email sending

--- Gemfile
+++ Gemfile
@@ -20,3 +20,8 @@ group :development, :test do
   gem 'spring'
 end
 
+group :production do
+  gem 'pg',             '0.17.1'
+  gem 'rails_12factor', '0.0.2'
+end
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = "blooming-mountain-4761.herokuapp.com/"
+  config.action_mailer.default_url_options = {host: host}
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => 'plain',
+      :username       => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_start_tls_auto => true
+  }
 end
